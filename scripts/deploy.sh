f884e55f650f4d1465050e18bacac28e7af99dc5
Brendan A John <brendanjohn@Brendans-MacBook-Pro.local>
2023-09-30 15:17:01 -0700

    npm build in deploy

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -3,5 +3,7 @@ rm  ../artifacts/lambda_function_payload.zip
 zip -r ../artifacts/lambda_function_payload.zip  .
 cd ../terraform
 terraform apply -auto-approve
-cd ..
-aws s3 cp webpage-src/index.html s3://summit-tournament-webhost-bucket/index.html
+cd ../webpage-src/summit 
+npm run build
+aws s3 cp --recursive build s3://summit-tournament-webhost-bucket
+
