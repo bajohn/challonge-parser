0f664eb368f71f573cd04ab607018156f1bb67fb
Brendan A John <brendanjohn@Brendans-MacBook-Pro.local>
2023-10-21 15:26:20 -0700

    fix sorting

--- webpage-src/summit/src/test/Podium.cy.tsx
+++ webpage-src/summit/src/test/Podium.cy.tsx
@@ -49,5 +49,67 @@ describe('<PodiumFinishes />', () => {
             });
 
     });
+
+    it('displays three players', () => {
+
+        const hardUrl = 'https://xx3ptt5y85.execute-api.us-west-2.amazonaws.com/summit-stage/podium-finishes';
+        const mockResp = {
+            statusCode: 200,
+            body: {
+                "data": {
+                    "First Place Finishes": {
+                        "playerA": 3,
+                        "playerC": 2,
+                        "playerB": 1,
+                    },
+                    "Second Place Finishes": {
+                        "playerC": 1,
+                        "playerA": 2,
+                        "playerB": 1,
+
+                    },
+                    "Third Place Finishes": {
+                        "playerC": 1,                      
+                        "playerA": 2,
+                        "playerB": 2
+                    }
+                }
+            }
+        };
+
+        cy.intercept(hardUrl, mockResp
+        ).as('getPodiums')
+            .then(async () => {
+                let podiums = {}
+                const setPodiums = (updated: any) => { podiums = updated };
+                await podiumGetter(setPodiums)
+                return podiums
+            })
+            .then((podiums) => {
+                console.log(podiums)
+                cy.mount(<PodiumFinishes podiums={podiums} />)
+                cy.get('tr td').each((el, idx) => {
+                    if (idx === 0) {
+                        cy.wrap(el).should('have.text', 'playerA');
+                    }
+                    else if (idx === 1) {
+                        cy.wrap(el).should('have.text', '🥇🥇🥇🥈🥈🥉🥉');
+                    }
+                    else if (idx === 2) {
+                        cy.wrap(el).should('have.text', 'playerC');
+                    }
+                    else if (idx === 3) {
+                        cy.wrap(el).should('have.text', '🥇🥇🥈🥉');
+                    }
+                    else if (idx === 4) {
+                        cy.wrap(el).should('have.text', 'playerB');
+                    }
+                    else if (idx === 5) {
+                        cy.wrap(el).should('have.text', '🥇🥈🥉🥉');
+                    }
+                })
+            });
+
+    });
 });
 
