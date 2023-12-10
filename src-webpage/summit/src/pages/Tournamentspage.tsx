b628970cc080f730f7dd6f9bcc05e5c91e43b975
Brendan A John <bjohn454@gmail.com>
2023-12-10 08:27:44 -0800

    tournament cards

--- src-webpage/summit/src/pages/Tournamentspage.tsx
+++ src-webpage/summit/src/pages/Tournamentspage.tsx
@@ -2,6 +2,7 @@ import React, { ReactNode, useEffect, useState } from "react";
 import { Container } from "react-bootstrap";
 import { tourneyGetter } from '../util/fetchers';
 import { iTournament } from '../../../../src-shared/types';
+import Card from 'react-bootstrap/Card';
 
 function TournamentsPage(props: any) {
     const [tourneys, setTourneys] = useState([] as iTournament[]);
@@ -23,11 +24,39 @@ function TournamentsPage(props: any) {
 
 const TournamentsBlock: React.FC<{ tourneys: iTournament[] }> = (props) => {
     console.log(props.tourneys);
+    props.tourneys.sort((a, b) => {
+        if (a.tournament.completed_at > b.tournament.completed_at) {
+            return -1;
+        } else {
+            return 1;
+        }
+    })
     const arr = props.tourneys.map((el: iTournament) => {
         console.log(el);
+        const challongeUrl = (suffix: string) => `https://challonge.com/${suffix}`;
         return (
             <Container key={el.tournament.id}>
-                {el.tournament.name}
+                <Card>
+                    <Card.Header as="h5">   {el.tournament.name}  </Card.Header>
+                    <Card.Body>
+                        <Card.Title></Card.Title>
+                        <Card.Text>
+                            <Container>
+                                Date started: {el.tournament.started_at.substring(0, 10)}
+                            </Container>
+                            <Container>
+                                Participants: {el.tournament.participants_count}
+                            </Container>
+                            <Container>
+                                Format: {el.tournament.tournament_type}
+                            </Container>
+                            <Container>
+                                <a href={challongeUrl(el.tournament.url)}>  Challonge Bracket </a>
+                            </Container>
+                        </Card.Text>
+                    </Card.Body>
+                </Card>
+
             </Container>
         )
     });
