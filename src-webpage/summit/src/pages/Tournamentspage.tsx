cedd56a43a5b04a7e67a501ad5d3be467cba6b33
Brendan A John <bjohn454@gmail.com>
2024-01-26 15:22:15 -0800

    admin link updates work better

--- src-webpage/summit/src/pages/Tournamentspage.tsx
+++ src-webpage/summit/src/pages/Tournamentspage.tsx
@@ -29,7 +29,7 @@ const TournamentsTable: React.FC<{
     isAdminPage: boolean
 }> = (props) => {
     props.tourneys.sort((a, b) => {
-        if (a.completed_at > b.completed_at) {
+        if (a.started_at > b.started_at) {
             return -1;
         } else {
             return 1;
@@ -47,13 +47,14 @@ const TournamentsTable: React.FC<{
     </Container>
 };
 
-const TournamentRow = (isAdminPage: boolean, el: iTournamentData) => {
+const TournamentRow = (isAdminPage: boolean, rowData: iTournamentData) => {
     const [READY, IN_PROG] = [1, 2];
     const [textInput, setTextInput] = useState("");
     const [updateState, setUpdateState] = useState(READY);
+    const [videoLink, setVideoLink] = useState(rowData.videoLink);
 
     const challongeUrl = (suffix: string) => `https://challonge.com/${suffix}`;
-    const rankedParticipants = el.rankedParticipants;
+    const rankedParticipants = rowData.rankedParticipants;
 
     const topThree: string[] = [];
 
@@ -72,27 +73,44 @@ const TournamentRow = (isAdminPage: boolean, el: iTournamentData) => {
         }, topThree)
     }
 
-    const AdminVideoEntry = (el: iTournamentData) => {
+    const AdminVideoEntry = (
+        tourneyId: number,
+        videoLink: string | undefined,
+        setVideoLink: React.Dispatch<React.SetStateAction<string | undefined>>
+    ) => {
         const onTextChange = (event: React.ChangeEvent<HTMLTextAreaElement>) => {
             setTextInput(event.target.value);
         }
 
         const onTextSubmit = async () => {
             setUpdateState(IN_PROG);
-            await updateTourney(el.id, { videoLink: textInput })
-            const updated = await getTourney(el.id);
-            el = updated;
+            await updateTourney(tourneyId, { videoLink: textInput })
+            const updated = await getTourney(tourneyId);
+            if (typeof updated.videoLink === 'string') {
+                setVideoLink(updated.videoLink);
+            } else {
+                setVideoLink(`Something went wrong while updating - new video link not found. ${updated.videoLink}`);
+            }
+
             setUpdateState(READY);
+
             // TODO - be nice to clean out the input text after submission.
-            // setTextInput('');
+            setTextInput('');
+        };
+
+        const onKeyPress: React.KeyboardEventHandler<any> = async (val) => {
+            if (val.key === 'Enter') {
+                onTextSubmit();
+            }
         }
+
         return <Container>
             {
-                el.videoLink ?
+                videoLink ?
                     <Container>
                         Current Link:
-                        <a href={el.videoLink}>
-                            {el.videoLink}
+                        <a href={videoLink} target="_blank">
+                            {videoLink}
                         </a>
                     </Container>
                     : <Container>
@@ -107,15 +125,14 @@ const TournamentRow = (isAdminPage: boolean, el: iTournamentData) => {
                     placeholder="Link to YouTube"
                     aria-label="Link to YouTube"
                     onChange={onTextChange}
+                    value={textInput}
+                    onKeyUp={onKeyPress}
                 />
                 {/* <InputGroup.Text
                         id="basic-addon2"
                         onChange={onTextChange}
                     >blahblah</InputGroup.Text> */}
             </InputGroup>
-
-
-
             {
                 updateState === READY ?
                     <Button onClick={onTextSubmit}>
@@ -130,11 +147,11 @@ const TournamentRow = (isAdminPage: boolean, el: iTournamentData) => {
         </Container>
     };
 
-    const VideoLink = (el: iTournamentData) => {
-        if (el.videoLink) {
+    const VideoLink = (videoLink: string | undefined) => {
+        if (videoLink) {
             return <Container>
-                <a target="_blank" href={el.videoLink}>
-                    {el.videoLink}
+                <a target="_blank" href={videoLink}>
+                    {videoLink}
                 </a>
             </Container>
         } else {
@@ -146,9 +163,9 @@ const TournamentRow = (isAdminPage: boolean, el: iTournamentData) => {
     };
 
     return (
-        <Container key={el.id}>
+        <Container key={rowData.id}>
             <Card>
-                <Card.Header as="h5">   {el.name}  </Card.Header>
+                <Card.Header as="h5">   {rowData.name}  </Card.Header>
                 <Card.Body>
                     {/* <Card.Title></Card.Title>
                     <Card.Text> */}
@@ -159,22 +176,22 @@ const TournamentRow = (isAdminPage: boolean, el: iTournamentData) => {
                     })}
 
                     <Container>
-                        Date: {el.started_at.substring(0, 10)}
+                        Date: {rowData.started_at.substring(0, 10)}
                     </Container>
                     <Container>
-                        Participants: {el.participants_count}
+                        Participants: {rowData.participants_count}
                     </Container>
                     <Container>
-                        Format: {pascalCase(el.tournament_type)}
+                        Format: {pascalCase(rowData.tournament_type)}
                     </Container>
                     <Container>
-                        <a target="_blank" href={challongeUrl(el.url)}>  Challonge Bracket </a>
+                        <a target="_blank" href={challongeUrl(rowData.url)}>  Challonge Bracket </a>
                     </Container>
 
                     {isAdminPage ?
-                        AdminVideoEntry(el)
+                        AdminVideoEntry(rowData.id, videoLink, setVideoLink)
                         :
-                        VideoLink(el)
+                        VideoLink(videoLink)
                     }
 
                     {/* </Card.Text> */}
