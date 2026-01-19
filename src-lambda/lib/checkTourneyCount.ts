d31e1230732b056fe86a75072f85ebe9d85e6ee5
Brendan A John <bjohn454@gmail.com>
2026-01-19 15:55:01 -0800

    add tourney complete check

--- src-lambda/lib/checkTourneyCount.ts
+++ src-lambda/lib/checkTourneyCount.ts
@@ -2,21 +2,24 @@ import { CHALLONGE_ENDPOINT } from "../../src-shared/constants";
 import { fetchTournies } from "./challongeFetch";
 import { apiSource, iTournament } from "../../src-shared/types";
 
-
 export const checkTourneyCount = async (source: apiSource) => {
-    const tourneys = await getTourneys(source);
-    const tourneyCount = tourneys.reduce((lv: number, cv: iTournament) => {
-        if (cv.tournament.state === 'complete') {
-            lv += 1;
-        }
-        return lv;
-    }, 0);
-    console.log(`Tournament count check, ${source}: ${tourneyCount}`);
-    return tourneyCount;
+  const tourneys = await getTourneys(source);
+  const tourneyCount = tourneys.reduce((lv: number, cv: iTournament) => {
+    if (cv.tournament.state === "complete") {
+      lv += 1;
+    }
+    return lv;
+  }, 0);
+  console.log(`Tournament count check, ${source}: ${tourneyCount}`);
+  return tourneyCount;
 };
 
-
 const getTourneys = async (source: apiSource) => {
-    const tourneys = await fetchTournies(CHALLONGE_ENDPOINT, source);
-    return tourneys;
+  const tourneys = await fetchTournies(CHALLONGE_ENDPOINT, source);
+  return tourneys.reduce((lv, cv) => {
+    if (cv.tournament.state === "complete") {
+      lv.push(cv);
+    }
+    return lv;
+  }, []);
 };
